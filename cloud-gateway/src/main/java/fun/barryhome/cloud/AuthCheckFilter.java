2c9559abe41b0abc0cca1c111528df173e275b7e
heyong <hypier@qq.com>
2020-08-15 22:54:30 +0800

    验证认证

---
+++ cloud-gateway/src/main/java/fun/barryhome/cloud/AuthCheckFilter.java
@@ -0,0 +1,68 @@
+package fun.barryhome.cloud;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * Created on 2020/8/15 9:35 下午
+ *
+ * @author barry
+ * Description:
+ */
+
+@Slf4j
+@Component
+public class AuthCheckFilter extends AbstractGatewayFilterFactory {
+
+    @Autowired
+    private Session session;
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            String token = request.getHeaders().getFirst("token");
+
+            log.info("AppAuthCheckFilter当前请求的url:{}", request.getURI().getPath());
+
+            if (Strings.isEmpty(token)) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            }
+
+            LoginUser loginUser = this.session.getSession(token);
+            if (loginUser == null) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            }
+
+            ServerWebExchange build;
+            try {
+                ServerHttpRequest host = exchange.getRequest().mutate()
+                        .header("X-User-Name", loginUser.userName)
+                        .header("X-Real-Name", URLEncoder.encode(loginUser.realName, "utf-8"))
+                        .build();
+                build = exchange.mutate().request(host).build();
+            } catch (UnsupportedEncodingException e) {
+                build = exchange;
+            }
+
+            return chain.filter(build);
+        };
+
+    }
+}
--- cloud-user/src/main/java/fun/barryhome/cloud/HomeController.java
+++ cloud-user/src/main/java/fun/barryhome/cloud/HomeController.java
@@ -1,7 +1,12 @@
 package fun.barryhome.cloud;
 
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  * Created on 2019-07-29 18:25
@@ -13,8 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping(value = "/home")
-    public String home() {
-        return "User Home !";
+    public String home(HttpServletRequest request) throws UnsupportedEncodingException {
+        String userName = request.getHeader("X-User-Name");
+        String realName = URLDecoder.decode(request.getHeader("X-Real-Name"), "utf-8");
+
+        return String.format("userName: %s, realName: %s", userName, realName);
     }
 
 
